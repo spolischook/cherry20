f6f231859033be5ad99e609b813407b3fb759c3f
Sergey Polischuk <spolischook@gmail.com>
2015-08-04 08:49:36 +0300

    Updated gulp flow

--- gulpfile.js
+++ gulpfile.js
@@ -17,30 +17,35 @@ gulp.task('minify-concat-css', function() {
         .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('inject', ['minify-concat-css'], function() {
+gulp.task('inject-prod', ['minify-concat-css'], function() {
     var styleFile = path.resolve('./css/' + cssFile);
-    var cssSha = checksum(fs.readFileSync(styleFile), 'sha1');
-    var target = gulp.src('./index.html');
-    var sources = gulp.src([styleFile], {read: false});
-
-    return target.pipe(inject(sources, {addSuffix: '?version='+cssSha}))
-        .pipe(gulp.dest('./'));
+    injectSources([styleFile]);
 });
 
-gulp.task('inject-styles', function() {
-    var target = gulp.src('./index.html');
-    var sources = gulp.src(['./css/**/*.css'], {read: false});
-
-    return target.pipe(inject(sources))
-        .pipe(gulp.dest('./'));
+gulp.task('inject-dev', function() {
+    injectSources(['./css/**/*.css']);
 });
 
-gulp.task('prod', ['minify-concat-css', 'inject']);
-gulp.task('dev', ['inject-styles']);
+gulp.task('prod', ['minify-concat-css', 'inject-prod']);
+gulp.task('dev', ['inject-dev']);
 
 function checksum (str, algorithm, encoding) {
     return crypto
         .createHash(algorithm || 'md5')
         .update(str, 'utf8')
         .digest(encoding || 'hex')
+}
+
+function injectSources(files) {
+    var target = gulp.src('./index.html');
+    var sources = gulp.src(files, {read: false});
+
+    return target.pipe(inject(sources, {
+        starttag: '"{{ext}}": [',
+        endtag: ']',
+        transform: function (filepath, file, i, length) {
+            var cssSha = checksum(fs.readFileSync('.'+filepath), 'sha1');
+            return '  "' + filepath + '?version=' + cssSha + '"' + (i + 1 < length ? ',' : '');
+        }
+    })).pipe(gulp.dest('./'));
 }
